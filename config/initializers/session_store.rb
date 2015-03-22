5b0743cefb5e94583d3356cf7574d6c33a9c61bd
Somendra Singh <somendra@Somendras.local>
2015-03-22 20:18:27 +0530

    x

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TheExplorerServices::Application.config.session_store :cookie_store, key: '_the_explorer_apis_session'
+TheExplorerApis::Application.config.session_store :cookie_store, key: '_the_explorer_apis_session'
