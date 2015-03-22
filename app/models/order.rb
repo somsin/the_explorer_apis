9c8c2183ba754199059b333c10afdb946c28746a
Somendra Singh <somendra@Somendras.local>
2015-03-22 22:05:57 +0530

    4 commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   self.table_name = 'order'
   
-  attr_accessible :user_id, :deal_id, :deal_count, :is_redeem, :voucher_code, :rating:integer
+  attr_accessible :user_id, :deal_id, :deal_count, :is_redeem, :voucher_code, :rating, :integer
   
   belongs_to :user , :class_name => "User"
 end
