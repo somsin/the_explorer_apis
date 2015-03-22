713ac6bc3055367c4f58ee2d8c8caa83c7c11417
Somendra Singh <somendra@Somendras.local>
2015-03-22 22:33:30 +0530

    x

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,8 @@
-require 'uuid_support'
 class Order < ActiveRecord::Base
-  include UUIDSupport
 
   self.table_name = 'order'
   
-  attr_accessible :user_id, :deal_id, :deal_count, :is_redeem, :voucher_code, :rating:integer
+  attr_accessible :user_id, :deal_id, :deal_count, :is_redeem, :voucher_code, :rating, :integer
   
   belongs_to :user , :class_name => "User"
 end
